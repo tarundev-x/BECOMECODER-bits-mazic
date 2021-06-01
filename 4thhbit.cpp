e6cf892d11dab959aa521484b4d7de73d63e3e98
tarundev-x <62273965+tarundev-x@users.noreply.github.com>
2021-06-01 01:55:15 -0700

    Add files via upload

---
+++ 4thhbit.cpp
@@ -0,0 +1,13 @@
+//fourth bit
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+ if((n>>3==1)&1==1)
+ cout<<"fourth bit is"<<1;
+ else
+ 	cout<<"fourth bit is"<<"0";
+	
+}
